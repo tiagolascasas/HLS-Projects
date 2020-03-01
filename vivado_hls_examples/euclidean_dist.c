5fb0f36e8b7180e8764f6728958da6be866f9ac9
tiagolascasas <tiagolascasas@hotmail.com>
2020-03-01 20:39:04 +0000

    rsqrt and sine/cosine tests

--- vivado_hls_examples/euclidean_dist.c
+++ vivado_hls_examples/euclidean_dist.c
@@ -1,10 +1,13 @@
 #include <math.h>
+#include "rsqrt.h"
 
 #define N 1024
 
 float euclidean_dist(float a[N], float b[N]) {
 	float dist = 0;
-	for (int i = 0; i < N; i++)
-		dist += powf(a[i] - b[i], 2);
+	for (int i = 0; i < N; i++) {
+		float d = a[i] - b[i];
+		dist += d * d;
+	}
 	return sqrtf(dist);
 }

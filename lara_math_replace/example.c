3af1e33f5f3792007caa38add160ca38c6cdc659
tiagolascasas <tiagolascasas@hotmail.com>
2020-03-07 01:26:20 +0000

    Started with LARA math.h strategy

---
+++ lara_math_replace/example.c
@@ -0,0 +1,35 @@
+#include <math.h>
+
+#define N 1024
+
+//Lara: should convert pow -> d * d and sqrt -> rsqrt32 
+float dist32(float x1[N], float x2[N]) {
+    float count = 0;
+    for (int i = 0; i < N; i++)
+        count += pow(x1[i] - x2[i], 2);
+    return sqrt(count);
+}
+
+//Lara: should convert sqrt -> rsqrt64
+double dist64(double x1[N], double x2[N]) {
+    double count = 0;
+    for (int i = 0; i < N; i++)
+        count += pow(x1[i] - x2[i], 2);
+    return sqrt(count);
+}
+
+//Lara: should convert sin -> sinf
+float sum_sin(float coef[N], float weight[N], float x) {
+    float sum = 0;
+    for (int i = 0; i < N; i++)
+        sum += weight[N] * sin(weight[N] * x)
+    return sum;
+}
+
+//Lara: should convert cos -> cosf
+float sum_cos(float coef[N], float weight[N], float x) {
+    float sum = 0;
+    for (int i = 0; i < N; i++)
+        sum += weight[N] * cos(weight[N] * x)
+    return sum;
+}

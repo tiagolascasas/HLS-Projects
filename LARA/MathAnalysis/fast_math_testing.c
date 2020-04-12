b0ce866316c11d99e7f1afe1d796cf000e508bfb
Tiago Lascasas dos Santos <tiagolascasas@hotmail.com>
2020-04-12 22:10:57 +0100

    Merged all fast math functions in a single header file

--- LARA/MathAnalysis/fast_math_testing.c
+++ LARA/MathAnalysis/fast_math_testing.c
@@ -1,70 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
-
-float fast_fabs(float a) {
-    unsigned int p2 = * (unsigned int *) &a; 
-    p2 = p2 & 0X7FFFFFFF;
-    return * (float *) &p2;
-}
-
-float fast_symmetrical(float a) {
-    unsigned int p2 = * (unsigned int *) &a; 
-    p2 = p2 ^ (1 << 31);  // p2 ^ 0X80000000
-    return * (float *) &p2;
-}
-
-float rsqrt32(float number) {
-	uint32_t i;
-	float x2, y;
-	x2 = number * 0.5F;
-	y = number;
-	i = * (uint32_t *) &y;
-	i = 0x5f375a86 - (i >> 1);
-	y = *(float *) &i;
-	y = y * (1.5F - (x2 * y * y));
-	return y;
-}
-
-double rsqrt64(double number) {
-	uint64_t i;
-	double x2, y;
-	x2 = number * 0.5;
-	y = number;
-	i = * (uint64_t *) &y;
-	i = 0x5fe6eb50c7b537a9 - (i >> 1);
-	y = * (double *) &i;
-	y = y * (1.5 - (x2 * y * y));
-	return y;
-}
+#include "fast_math.h"
 
 int main() {
     printf("fabs\n");
     float a = -1.345f;
-    float aa = fast_fabs(a);
+    float aa = fast_fabs32(a);
     printf("%f -> %f\n", a, aa);
     float b = 1.345f;
-    float bb = fast_fabs(b);
+    float bb = fast_fabs32(b);
     printf("%f -> %f\n", b, bb);
 
     printf("symmetrical\n");
     float c = 2.450f;
-    float cc = fast_symmetrical(c);
+    float cc = fast_symmetrical32(c);
     printf("%f -> %f\n", c, cc);
     float d = -2.450f;
-    float dd = fast_symmetrical(dd);
+    float dd = fast_symmetrical32(dd);
     printf("%f -> %f\n", d, dd);
 
     printf("sqrt float\n");
     float e = 1256.45f;
     float e1 = sqrtf(e);
-    float e2 = e * rsqrt32(e);
+    float e2 = e * fast_inv_sqrt32(e);
     printf("%f -> %f | %f\n", e, e1, e2);
 
     printf("sqrt double\n");
     double f = 1256.45;
     double f1 = sqrt(f);
-    double f2 = f * rsqrt64(f);
+    double f2 = f * fast_inv_sqrt64(f);
     printf("%f -> %f | %f\n", f, f1, f2);
 
     return 0;
